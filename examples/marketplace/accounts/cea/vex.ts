ff20a8973e074d2c03298f5b77714f8394cc10a3
Guilherme Rodrigues <guilherme@vtex.com>
2021-11-12 19:44:53 -0300

    Improve readme

--- examples/marketplace/accounts/cea/vex.ts
+++ examples/marketplace/accounts/cea/vex.ts
@@ -1,14 +1,11 @@
 import { Pricing } from "../../apps/vtex.pricing@1/app.vex.ts"
 import { Logistics } from "../../apps/vtex.logistics@1/app.vex.ts"
 import { Storefront } from "./storefronts/brasil.vex.ts"
+import { config as checkout } from "./checkout/checkout.vex.ts"
 import { VTEXAccount } from "../../apps/vtex.account@1/app.vex.ts"
 
-const config = new CheckoutConfiguration({
-    timeToCancel: 20
-})
-
 export default new VTEXAccount({
-    checkout: Checkout,
+    checkout,
     pricing: Pricing,
     logistics: Logistics,
     storefronts: [Storefront]
